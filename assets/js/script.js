a8ed3cddcefc44e94cb407863577615860aec829
duongnguyen185 <duong.nguyen@ftribe.games>
2022-10-23 22:43:59 +0700

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,157 @@
+"use strict";
+
+// add event on multiple elements
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// navbar functionality
+
+const [navbar, navToggler, navbarLinks] = [
+  document.querySelector("[data-navbar]"),
+  document.querySelector("[data-nav-toggler]"),
+  document.querySelectorAll("[data-nav-link]"),
+];
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+  document.body.classList.toggle("active");
+};
+
+navToggler.addEventListener("click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+  document.body.classList.remove("active");
+};
+
+addEventOnElements(navbarLinks, "click", closeNavbar);
+
+// header active
+
+const header = document.querySelector("[data-header]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", activeElemOnScroll);
+
+// scroll reveal effect
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealOnScroll = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    // add revealed class on element, when visible in window
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight / 1.1
+    ) {
+      revealElements[i].classList.add("revealed");
+
+      // remove long transition from button, after 1 second
+      if (revealElements[i].classList.contains("btn")) {
+        setTimeout(function () {
+          revealElements[i].style.transition = "0.25s ease";
+        }, 1000);
+      }
+    }
+  }
+};
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
+
+let player1;
+let player2;
+let player3;
+let player4;
+
+let card = document.getElementById("video-card-1");
+let video = document.getElementById("video-container-1");
+
+let card2 = document.getElementById("video-card-2");
+let video2 = document.getElementById("video-container-2");
+
+let card3 = document.getElementById("video-card-3");
+let video3 = document.getElementById("video-container-3");
+
+let card4 = document.getElementById("video-card-4");
+let video4 = document.getElementById("video-container-4");
+
+function onYouTubePlayerAPIReady() {
+  player1 = new YT.Player("video1", {
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+  player2 = new YT.Player("video2", {
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+  player3 = new YT.Player("video3", {
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+  player4 = new YT.Player("video4", {
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}
+
+function onPlayerReady(event) {
+  // bind events
+  let playButton1 = document.getElementById("play-button1");
+  playButton1.addEventListener("click", function () {
+    card.classList.add("video-is-open");
+    setTimeout(function () {
+      video.style.display = "block";
+      player1.playVideo();
+    }, 500);
+  });
+
+  let playButton2 = document.getElementById("play-button2");
+  playButton2.addEventListener("click", function () {
+    card2.classList.add("video-is-open2");
+    setTimeout(function () {
+      video2.style.display = "block";
+      player2.playVideo();
+    }, 500);
+  });
+  let playButton3 = document.getElementById("play-button3");
+  playButton3.addEventListener("click", function () {
+    card3.classList.add("video-is-open3");
+    setTimeout(function () {
+      video3.style.display = "block";
+      player3.playVideo();
+    }, 500);
+  });
+  let playButton4 = document.getElementById("play-button4");
+  playButton4.addEventListener("click", function () {
+    card4.classList.add("video-is-open4");
+    setTimeout(function () {
+      video4.style.display = "block";
+      player4.playVideo();
+    }, 500);
+  });
+}
+
+// Inject YouTube API script
+var tag = document.createElement("script");
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
